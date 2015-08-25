a8146fac7921996df212a501e82e83a16e96f788
Eduardo Gorio <goriofab@gmail.com>
2015-08-25 09:39:56 -0300

    Update AndroidUtils.java

--- AndroidUtils.java
+++ AndroidUtils.java
@@ -102,4 +102,157 @@ IntentFilter filter = new IntentFilter();
 filter.addAction("android.settings.SYSTEM_UPDATE_SETTINGS");
 registerReceiver(sysUpdate, filter);
 
+// EXCEL
+// AndroidManifest.xml: <uses-permissionandroid:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+// Gradle: compile 'org.apache.poi:poi:3.9'
+
+    /**
+     * Save data to Excel file
+     * @param context
+     * @param fileName (i.e. myExcel.xls)
+     * @return boolean
+     */
+    private static boolean saveExcelFile(Context context, String fileName) {
+
+        // check if available and not read only
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            Log.e(TAG, "Storage not available or read only");
+            return false;
+        }
+
+        boolean success = false;
+
+        //New Workbook
+        Workbook wb = new HSSFWorkbook();
+
+        Cell c = null;
+
+        //Cell style for header row
+        CellStyle cs = wb.createCellStyle();
+        cs.setFillForegroundColor(HSSFColor.LIME.index);
+        cs.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+
+        //New Sheet
+        Sheet sheet1 = null;
+        sheet1 = wb.createSheet("myOrder");
+
+        // Generate column headings
+        Row row = sheet1.createRow(0);
+        Row row1 = sheet1.createRow(1);
+
+        c = row.createCell(0);
+        c.setCellValue("Item Number");
+        c.setCellStyle(cs);
+
+        c = row.createCell(1);
+        c.setCellValue("Quantity");
+        c.setCellStyle(cs);
+
+        c = row.createCell(2);
+        c.setCellValue("Price");
+        c.setCellStyle(cs);
+
+        c = row1.createCell(0);
+        c.setCellValue("1");
+
+        c = row1.createCell(1);
+        c.setCellValue("10");
+
+        c = row1.createCell(2);
+        c.setCellValue("20,25");
+
+        sheet1.setColumnWidth(0, (15 * 500));
+        sheet1.setColumnWidth(1, (15 * 500));
+        sheet1.setColumnWidth(2, (15 * 500));
+
+        // Create a path where we will place our List of objects on external storage
+        File file = new File(context.getExternalFilesDir(null), fileName);
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(file);
+            wb.write(os);
+            Log.w("FileUtils", "Writing file" + file);
+            success = true;
+        } catch (IOException e) {
+            Log.w("FileUtils", "Error writing " + file, e);
+        } catch (Exception e) {
+            Log.w("FileUtils", "Failed to save file", e);
+        } finally {
+            try {
+                if (null != os)
+                    os.close();
+            } catch (Exception ex) {
+            }
+        }
+        return success;
+    }
+
+    /**
+     * Save data to Excel file
+     * @param context
+     * @param fileName (i.e. myExcel.xls)
+     */
+    private static void readExcelFile(Context context, String fileName) {
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly())
+        {
+            Log.e(TAG, "Storage not available or read only");
+            return;
+        }
+
+        try{
+            // Creating Input Stream
+            File file = new File(context.getExternalFilesDir(null), fileName);
+            FileInputStream myInput = new FileInputStream(file);
+
+            // Create a POIFSFileSystem object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+            /** We now need something to iterate through the cells.**/
+            Iterator rowIter = mySheet.rowIterator();
+
+            while(rowIter.hasNext()){
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                Iterator cellIter = myRow.cellIterator();
+                while(cellIter.hasNext()){
+                    HSSFCell myCell = (HSSFCell) cellIter.next();
+                    Log.d(TAG, "Cell Value: " +  myCell.toString());
+                    Toast.makeText(context, "cell Value: " + myCell.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }catch (Exception e){e.printStackTrace(); }
+
+        return;
+    }
+
+    /**
+     * Check status of External Storage
+     * @return boolean
+     */
+    public static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Check if External Storage is Available
+     * @return boolean
+     */
+    public static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
 }
