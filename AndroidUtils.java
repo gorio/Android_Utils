54f62b1c2e15d6461032e502fa57ba7795d8a410
Eduardo Gorio <goriofab@gmail.com>
2015-09-08 11:07:37 -0300

    Update AndroidUtils.java

--- AndroidUtils.java
+++ AndroidUtils.java
@@ -255,4 +255,12 @@ registerReceiver(sysUpdate, filter);
         }
         return false;
     }
+    
+    // Sending side
+    byte[] data = text.getBytes("UTF-8");
+    String base64 = Base64.encodeToString(data, Base64.DEFAULT);
+    
+    // Receiving side
+    byte[] data = Base64.decode(base64, Base64.DEFAULT);
+    String text = new String(data, "UTF-8");
 }
