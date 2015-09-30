419722d7e46df8a071db9ca57ac966cd30b2f8c5
Eduardo Gorio <goriofab@gmail.com>
2015-09-30 16:27:40 -0300

    Update AndroidUtils.java

--- AndroidUtils.java
+++ AndroidUtils.java
@@ -1,5 +1,40 @@
 public class AndroidUtils {
 
+  /**
+     * Process Info
+     */
+    public static void processInfo() {
+
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+
+        Log.i(TAG, " memoryInfo.availMem " + memoryInfo.availMem + "\n");
+        Log.i(TAG, " memoryInfo.lowMemory " + memoryInfo.lowMemory + "\n");
+        Log.i(TAG, " memoryInfo.threshold " + memoryInfo.threshold + "\n");
+
+        List<RunningAppProcessInfo> runningAppProcesses = activityManager.getRunningAppProcesses();
+
+        Map<Integer, String> pidMap = new TreeMap<Integer, String>();
+        for (RunningAppProcessInfo runningAppProcessInfo : runningAppProcesses) {
+            pidMap.put(runningAppProcessInfo.pid, runningAppProcessInfo.processName);
+        }
+
+        Collection<Integer> keys = pidMap.keySet();
+
+        for (int key : keys) {
+            int pids[] = new int[1];
+            pids[0] = key;
+            android.os.Debug.MemoryInfo[] memoryInfoArray = activityManager.getProcessMemoryInfo(pids);
+            for (android.os.Debug.MemoryInfo pidMemoryInfo : memoryInfoArray) {
+                Log.i(TAG, String.format("** MEMINFO in pid %d [%s] **\n", pids[0], pidMap.get(pids[0])));
+                Log.i(TAG, " pidMemoryInfo.getTotalPrivateDirty(): " + pidMemoryInfo.getTotalPrivateDirty() + "\n");
+                Log.i(TAG, " pidMemoryInfo.getTotalPss(): " + pidMemoryInfo.getTotalPss() + "\n");
+                Log.i(TAG, " pidMemoryInfo.getTotalSharedDirty(): " + pidMemoryInfo.getTotalSharedDirty() + "\n");
+            }
+        }
+    }
+  
   
   /**
    * Obtain all PreInstalled Apps
